58d7784cb09ca1e4502a6499dc84a2b9083dd658
sousafilp <sousaafilp@gmail.com>
2024-05-07 10:20:17 -0300

    exemplo de variáveis de ambiente utilizadas

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor escutando na porta ${PORT}!`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor escutando na porta ${process.env.PORT}!`);
 });
